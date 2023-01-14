dbefdda20d365b5d38a661de0cc1c054448627b1
Stanislau Virsky <vyr4376@yandex.ru>
2023-01-14 11:25:09 +0300

    feat: end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function toReadable (number) {
-  
-}
+     let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
+        'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+        'seventeen', 'eighteen', 'nineteen'];
+        let tens = ['','', 'twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+        let numStr = String(number);
+        if (number === 0) return 'zero'
+        if (number < 20 ) return ones[number];
+        if (number >= 20 && number < 100) return (tens[numStr.at(0)] + ' ' + ones[numStr.at(1)]).trim()
+        if (number >=100 && (numStr.at(1) + numStr.at(2) < 20)) return (ones[numStr.at(0)] + ' hundred' + ' ' + ones[+(numStr.at(1) + numStr.at(2))]).trim();
+        if (number >= 100) return (ones[numStr.at(0)] + ' hundred ' + tens[numStr.at(1)] + ' ' + ones[numStr.at(2)]).trim();
+
+       
+        }
+
+
+
