e5fa7faa0326ded38fb8ebf20797037947700f2e
Stanislau Virsky <114683300+W0x3R@users.noreply.github.com>
2023-01-14 13:09:05 +0300

    feat : add new idea

--- src/index.js
+++ src/index.js
@@ -13,5 +13,17 @@ module.exports = function toReadable (number) {
        
         }
 
+/*  можно и так)
+let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
+        'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+        'seventeen', 'eighteen', 'nineteen'];
+        let tens = ['','', 'twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+        let numStr = String(number);
+        if (number === 0) return 'zero'
+        if (number < 20 ) return ones[number];
+        if (numStr.length === 2) return (tens[numStr.at(0)] + ' ' + ones[numStr.at(1)]).trim();
+        if (numStr.length === 3 && (numStr.at(1) + numStr.at(2)) <20 )  return (ones[numStr.at(0)] + ' hundred ' + ones[+(numStr.at(1) + numStr.at(2))]).trim()
+        if (numStr.length === 3) return (ones[numStr.at(0)] +  ' hundred ' +  tens[numStr.at(1)] + ' ' + ones[numStr.at(2)]).trim();
 
+*/
 
